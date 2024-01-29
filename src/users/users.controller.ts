d29e4cbce4532fe1889069f791af272ac2c3d400
Riyas Iqbal <riyasiqbal10@gmail.com>
2024-01-29 23:00:40 +0530

    creating services and routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,43 @@
-import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    
+    /**
+     * This automatically creates a new instance for userService and it is also mentioned in the
+     * documentation that if this instance was created elsewhere it will pull the instance from there
+     * following the singleton principle 
+     */
+    constructor(private readonly userService: UsersService) {}
+
     @Get() // eg: /users?role=admin
-    findAll(@Query('role') role: 'INTERN' | 'ADMIN'){
-        console.log(role);
-        return 'Hello world'
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        return this.userService.findAll(role)
     }
 
     @Get('/interns') //eg: /users/interns
     findInters(){
-        return 'Hello interns'
+        return this.userService.findAll('INTERN')
     }
 
     @Get(':id')
     findById(@Param('id') id: string){
-        return 'id-' + id 
+        return this.userService.findOne(parseInt(id))
     } 
 
     @Post()
-    create(@Body() user: {} ){
-        return user
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN', age: number } ){
+        return this.userService.create(user)
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {} ){
-        return {id, ...userUpdate}
+    update(@Param('id') id: string, @Body() userUpdate: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN', age: number } ){
+        return this.userService.update(parseInt(id), userUpdate)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return this.userService.delete(parseInt(id))
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,82 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    // Sample Data from DB
+    private users = [
+        {
+            id: 1,
+            name: 'John',
+            age: 31,
+            email: 'john@example.com',
+            role: 'ADMIN'
+        }, {
+            id: 2,
+            name: 'Alex',
+            age: 20,
+            email: 'Alex@example.com',
+            role: 'INTERN'
+        }, {
+            id: 3,
+            name: 'Harry',
+            age: 40,
+            email: 'harry@example.com',
+            role: 'ENGINEER'
+        }, {
+            id: 4,
+            name: 'Nancy',
+            age: 18,
+            email: 'nancy@example.com',
+            role: 'INTERN'
+        }, {
+            id: 5,
+            name: 'Georgia',
+            age: 21,
+            email: 'grg@example.com',
+            role: 'INTERN'
+        }
+    ]
+    
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id)
+        if (user) {
+            return user
+        }
+
+        return 'User doesnt exist with id:' + id
+    }
+
+    create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN', age: number }) {
+        const userByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+        const highestId = userByHighestId[0].id
+        const newUser = {
+            id: highestId + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updateUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN', age?: number }) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updateUser }
+            }
+            return user
+        })
+
+        return this.findOne(id)
+    }
+
+    delete(id: number) {
+        const removeUser = this.findOne(id)
+        this.users = this.users.filter(user => user.id !== id)
+    }
+} 
