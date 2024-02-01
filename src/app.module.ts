61a011d3e3456424a5acd41678b92fa4da04abf1
Riyas Iqbal <riyasiqbal10@gmail.com>
2024-02-02 02:28:06 +0530

    Application level configuration and API Throttling

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,30 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
+const THROTTLE_OPTIONS = [{
+  name: "short",
+  ttl: 1 * 1000,
+  limit: 3,
+},{
+  name: "long",
+  ttl: 10 * 1000,
+  limit: 50,
+}]
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule, 
+    DatabaseModule, 
+    EmployeesModule,
+    ThrottlerModule.forRoot(THROTTLE_OPTIONS)
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
